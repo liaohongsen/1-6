cfeb12f50cc9953b12d44234c1a2c062aee903db
lhs <1115317176@qq.com>
2026-02-05 22:18:14 +0800

    ss

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,17 +42,17 @@ export const Footer: React.FC = () => {
           <div className="bg-white p-0.5 rounded-lg shadow-sm">
             <div className="w-14 h-14 rounded-md overflow-hidden relative">
                <img 
-                 src="https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=YUSHI-CONSULTING&color=e67e22" 
+                 src="./images/wechat.jpg" 
                  alt="QR Code" 
                  className="w-full h-full object-cover opacity-90"
                />
-               <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+               {/* <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
                  <div className="w-3.5 h-3.5 bg-white rounded-sm flex items-center justify-center shadow-xs">
                     <svg viewBox="0 0 24 24" className="w-2.5 h-2.5 fill-[#07C160]">
                       <path d="M8.5,13.5C9.33,13.5 10,12.83 10,12C10,11.17 9.33,10.5 8.5,10.5C7.67,10.5 7,11.17 7,12C7,12.83 7.67,13.5 8.5,13.5M15.5,13.5C16.33,13.5 17,12.83 17,12C17,11.17 16.33,10.5 15.5,10.5C14.67,10.5 14,11.17 14,12C14,12.83 14.67,13.5 15.5,13.5M12,2A10,10 0 0,0 2,12C2,13.31 2.25,14.56 2.71,15.7L1,21L6.65,19.64C7.67,20.5 8.78,21.18 10,21.61L10,21C10,15.47 14.47,11 20,11L20.61,11C21.18,9.78 22,8.67 22,7.5A10,10 0 0,0 12,2Z" />
                     </svg>
                  </div>
-               </div>
+               </div> */}
             </div>
           </div>
         </div>
